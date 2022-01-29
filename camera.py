95b9072f727d7eea41bf3600b7f2d074bf9db759
NandaNxD <nanven9@gmail.com>
2022-01-29 22:07:01 +0530

    Working Camera Class

--- camera.py
+++ camera.py
@@ -3,16 +3,19 @@ import os
 
 
 class Camera:
-    PATH_TO_IMAGE = '/home'
+    IMAGE_NAME = 'image'
     camera = 1
 
     def __init__(self, PATH_TO_IMAGE):
         self.PATH_TO_IMAGE = PATH_TO_IMAGE
         self.camera = PiCamera()
         self.camera.rotation = 180
+    def __init(self):
+        self.camera=PiCamera()
+        self.camera.rotation=180
 
     def capture(self):
-        self.camera.capture(self.PATH_TO_IMAGE)
+        self.camera.capture(output=self.IMAGE_NAME,format='jpeg')
 
-    def __del__(self):
-        self.camera.close()
+    def capure(self,OUTPUT):
+        self.camera.capture(output=OUTPUT,format='jpeg')
